a4731e91c59d1f61a97ab4ca1b2d566cefca0c94
blueWisdom09 <758793079@qq.com>
2022-06-10 15:19:45 +0800

    买卖股票的最佳时机 II:贪心算法简化代码

--- algorithm-practice/src/algorithm/array/MaxProfitArray.java
+++ algorithm-practice/src/algorithm/array/MaxProfitArray.java
@@ -30,14 +30,15 @@ public class MaxProfitArray {
         day[0][0] = 0;
         day[0][1] = -prices[0];
         for (int i = 1; i < prices.length; i++) {
-            day[i][0] = Integer.max(day[i-1][0],day[i-1][1] +prices[i]);
-            day[i][1] = Integer.max(day[i-1][1],day[i-1][0] - prices[i]);
+            day[i][0] = Integer.max(day[i - 1][0], day[i - 1][1] + prices[i]);
+            day[i][1] = Integer.max(day[i - 1][1], day[i - 1][0] - prices[i]);
         }
         return day[prices.length][0];
     }
 
     /**
      * 动态规划优化版（二维数组转为变量）
+     *
      * @param prices
      * @return int
      * @author chenjianhua
@@ -55,8 +56,8 @@ public class MaxProfitArray {
         int hold = -prices[0];
         int throwHold = 0;
         for (int i = 1; i < prices.length; i++) {
-            throwHold = Integer.max(throwHold,hold +prices[i]);
-            hold = Integer.max(hold,throwHold - prices[i]);
+            throwHold = Integer.max(throwHold, hold + prices[i]);
+            hold = Integer.max(hold, throwHold - prices[i]);
         }
         return throwHold;
     }
@@ -64,28 +65,45 @@ public class MaxProfitArray {
 
     /**
      * 贪心算法 找到上涨前的最小值和上涨最大值，取差值
+     *
      * @param prices
      * @return int
      * @author chenjianhua
      * @date 2022/6/9 13:42
      */
     public int maxProfitThree(int[] prices) {
-        if (prices == null || prices.length < 2){
+        if (prices == null || prices.length < 2) {
             return 0;
         }
         int index = 0;
         int total = 0;
-        while(index < prices.length){
+        while (index < prices.length) {
             //上涨前最小值
-            while(index < prices.length - 1 && prices[index] >= prices[index + 1]) {
+            while (index < prices.length - 1 && prices[index] >= prices[index + 1]) {
                 index++;
             }
             int min = prices[index];
             //上涨峰值
-            while(index < prices.length - 1 && prices[index]<=prices[index + 1]){
+            while (index < prices.length - 1 && prices[index] <= prices[index + 1]) {
                 index++;
             }
             total += prices[index++] - min;
+        }
+        return total;
+    }
+
+    /**
+     * 贪心算法,直接收集正收入，求和，得到最终利润
+     *
+     * @param prices
+     * @return int
+     * @author chenjianhua
+     * @date 2022/6/10 15:16
+     */
+    public int maxProfitFour(int[] prices) {
+        int total = 0;
+        for (int i = 1; i < prices.length; i++) {
+            total += Math.max(0, prices[i] - prices[i - 1]);
         }
         return total;
     }
