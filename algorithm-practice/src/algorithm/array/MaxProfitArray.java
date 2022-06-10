e5fd4e258ede40bb882c17f59d9133e589430d52
blueWisdom09 <758793079@qq.com>
2022-06-10 15:06:42 +0800

    数组：删除排序数组中的重复项、买卖股票的最佳时机 II

---
+++ algorithm-practice/src/algorithm/array/MaxProfitArray.java
@@ -0,0 +1,92 @@
+package algorithm.array;
+
+/**
+ * @author chenjianhua
+ * @Description 买卖股票的最佳时机 II （在任何时候 最多 只能持有 一股 股票）
+ * @See https://leetcode.cn/leetbook/read/top-interview-questions-easy/x2zsx1/
+ * @title MaxProfitArray
+ * @date 2022/6/9
+ */
+public class MaxProfitArray {
+
+    /**
+     * 动态规划
+     *
+     * @param prices
+     * @return int
+     * @author chenjianhua
+     * @date 2022/6/9 10:54
+     */
+    public int maxProfitOne(int[] prices) {
+        //排除异常情况
+        if (prices == null || prices.length < 2) {
+            return 0;
+        }
+        int[][] day = new int[prices.length][2];
+        //当天未持有股票，利润:1、未发生任何交易，2、将股票卖出（昨天一定将购入股票） day[i][0] = max(day[i-1][0]，day[i-1][1]+prices[i]);
+        //当天购入股票后，利润:1、未发生任何交易,2、买入股票（昨天一定将股票卖出） day[i][1] = max(day[i-1][1],day[i-1][0]-prices[i]);
+        //最终结果一定是当天未持有股票的利润高
+        //初始值
+        day[0][0] = 0;
+        day[0][1] = -prices[0];
+        for (int i = 1; i < prices.length; i++) {
+            day[i][0] = Integer.max(day[i-1][0],day[i-1][1] +prices[i]);
+            day[i][1] = Integer.max(day[i-1][1],day[i-1][0] - prices[i]);
+        }
+        return day[prices.length][0];
+    }
+
+    /**
+     * 动态规划优化版（二维数组转为变量）
+     * @param prices
+     * @return int
+     * @author chenjianhua
+     * @date 2022/6/9 13:16
+     */
+    public int maxProfitTwo(int[] prices) {
+        //排除异常情况
+        if (prices == null || prices.length < 2) {
+            return 0;
+        }
+        //当天未持有股票，利润:1、未发生任何交易，2、将股票卖出 day[i][0] = max(day[i-1][0]，day[i-1][1]+prices[i]);
+        //当天购入股票后，利润:1、未发生任何交易,2、买入股票 day[i][1] = max(day[i-1][1],day[i-1][0]-prices[i]);
+        //最终结果一定是当天未持有股票的利润高
+        //初始值
+        int hold = -prices[0];
+        int throwHold = 0;
+        for (int i = 1; i < prices.length; i++) {
+            throwHold = Integer.max(throwHold,hold +prices[i]);
+            hold = Integer.max(hold,throwHold - prices[i]);
+        }
+        return throwHold;
+    }
+
+
+    /**
+     * 贪心算法 找到上涨前的最小值和上涨最大值，取差值
+     * @param prices
+     * @return int
+     * @author chenjianhua
+     * @date 2022/6/9 13:42
+     */
+    public int maxProfitThree(int[] prices) {
+        if (prices == null || prices.length < 2){
+            return 0;
+        }
+        int index = 0;
+        int total = 0;
+        while(index < prices.length){
+            //上涨前最小值
+            while(index < prices.length - 1 && prices[index] >= prices[index + 1]) {
+                index++;
+            }
+            int min = prices[index];
+            //上涨峰值
+            while(index < prices.length - 1 && prices[index]<=prices[index + 1]){
+                index++;
+            }
+            total += prices[index++] - min;
+        }
+        return total;
+    }
+}
---
+++ algorithm-practice/src/algorithm/array/RemoveDuplicateArray.java
@@ -0,0 +1,63 @@
+package algorithm.array;
+
+/**
+ * @author chenjianhua
+ * @Description 删除排序数组中的重复项
+ * @See https://leetcode.cn/leetbook/read/top-interview-questions-easy/x2gy9m/
+ * @title RemoveDuplicateArray
+ * @date 2022/6/6
+ */
+public class RemoveDuplicateArray {
+
+    /**
+     * 双指针思路
+     * @param nums
+     * @return int
+     * @author chenjianhua
+     * @date 2022/6/9 10:54
+     */
+    public int removeDuplicatesOne(int[] nums) {
+        //空数组校验
+        if (nums == null || nums.length == 0){
+            return 0;
+        }
+        //前提：排序数组.
+        //思路：双指针处理：left指向第一个元素，right指向第二个元素。
+        // 1,若两者指向元素不同,right指向的元素赋值给left指向元素的下一个元素,left+1
+        // 2,若两者指向元素相等，left不变，right+1，指向下一个元素，再进行比较操作。
+        // 这样交换可实现O(1)复杂度的算法
+        int left = 0;
+        for(int right = 1; right < nums.length; right++ ){
+            if (nums[left] != nums[right]){
+                nums[++left] = nums[right];
+            }
+        }
+        return ++left;
+    }
+
+    /**
+     * 直接交换位置思路
+     * @param nums
+     * @return int
+     * @author chenjianhua
+     * @date 2022/6/9 10:54
+     */
+    public int removeDuplicatesTwo(int[] nums) {
+        //空数组校验
+        if (nums == null || nums.length == 0){
+            return 0;
+        }
+        //前提：排序数组.
+        //思路：直接交换位置：将重复数量记录，不重复时，将第一个重复的数和当前数交换
+        //重复的数一次次的后移
+        int count = 0;
+        for(int right = 1; right < nums.length; right++ ){
+            if (nums[right] == nums[right - 1]){
+                count++;
+            } else{
+                nums[right - count] = nums[right];
+            }
+        }
+        return nums.length - count;
+    }
+}
