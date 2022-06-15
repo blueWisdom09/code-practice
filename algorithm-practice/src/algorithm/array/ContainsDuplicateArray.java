3784bbe8ffa966fa2beec6003443bf45658673d6
blueWisdom09 <758793079@qq.com>
2022-06-15 18:01:22 +0800

    两个数组的交集II

--- algorithm-practice/src/algorithm/array/ContainsDuplicateArray.java
+++ algorithm-practice/src/algorithm/array/ContainsDuplicateArray.java
@@ -28,6 +28,7 @@ public class ContainsDuplicateArray {
      * @date 2022/6/14 14:34
      */
     public static boolean containsDuplicateOne(int[] nums) {
+        //双层for循环
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
                 if(nums[i] == nums[j]){
---
+++ algorithm-practice/src/algorithm/array/IntersectArray.java
@@ -0,0 +1,121 @@
+package algorithm.array;
+
+import java.util.*;
+
+/**
+ * @author chenjianhua
+ * @Description 两个数组的交集II
+ * @title IntersectArray
+ * @date 2022/6/15
+ */
+public class IntersectArray {
+
+
+    /**
+     * 先排序再比较，双指针移动
+     *
+     * @param nums1
+     * @param nums2
+     * @return int[]
+     * @author chenjianhua
+     * @date 2022/6/15 16:25
+     */
+    public int[] intersectOne(int[] nums1, int[] nums2) {
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        int i = 0;
+        int j = 0;
+        List<Integer> list = new ArrayList<>();
+        while (i < nums1.length && j < nums2.length) {
+            //哪个值小，哪个指针就后移一位
+            if (nums1[i] < nums2[j]) {
+                i++;
+            } else if (nums1[i] > nums2[j]) {
+                j++;
+            } else {
+                //重复，同时后移
+                list.add(nums1[i]);
+                i++;
+                j++;
+            }
+        }
+        int index = 0;
+        int[] resultArray = new int[list.size()];
+        for (Integer integer : list) {
+            resultArray[index] = integer;
+            index++;
+        }
+        return resultArray;
+    }
+
+    /**
+     * 利用map的特性
+     *
+     * @param nums1
+     * @param nums2
+     * @return int[]
+     * @author chenjianhua
+     * @date 2022/6/15 16:44
+     */
+    public int[] intersectTwo(int[] nums1, int[] nums2) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums1.length; i++) {
+            //记录元素，以及在数组中出现的次数
+            map.put(nums1[i], map.getOrDefault(nums1[i], 0) + 1);
+        }
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < nums2.length; i++) {
+            if (map.getOrDefault(nums2[i], 0) > 0) {
+                list.add(nums2[i]);
+                //减少次数
+                map.put(nums2[i], map.get(nums2[i]) - 1);
+            }
+        }
+        int index = 0;
+        int[] resultArray = new int[list.size()];
+        for (Integer integer : list) {
+            resultArray[index] = integer;
+            index++;
+        }
+        Arrays.copyOfRange(resultArray, 0, index);
+        return resultArray;
+    }
+
+    /**
+     * 官方优化版本 map
+     * @param nums1
+     * @param nums2
+     * @return int[]
+     * @author chenjianhua
+     * @date 2022/6/15 17:36
+     */
+    public int[] intersectThree(int[] nums1, int[] nums2) {
+        if (nums1.length > nums2.length) {
+            return intersectThree(nums2, nums1);
+        }
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums1.length; i++) {
+            //记录元素，以及在数组中出现的次数
+            int count = map.getOrDefault(nums1[i], 0) + 1;
+            map.put(nums1[i], count);
+        }
+        int[] resultArray = new int[nums1.length];
+        int index = 0;
+        for (int i = 0; i < nums2.length; i++) {
+            int count = map.getOrDefault(nums2[i], 0);
+            if (count > 0) {
+                resultArray[index++] = nums2[i];
+                //减少次数
+                count--;
+                if (count > 0) {
+                    map.put(nums2[i], count);
+                } else {
+                    //清除元素
+                    map.remove(nums2[i]);
+                }
+            }
+        }
+        return Arrays.copyOfRange(resultArray, 0, index);
+    }
+
+}
