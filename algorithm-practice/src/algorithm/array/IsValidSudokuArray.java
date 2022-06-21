eb299be0e4c32fd3e380277167aec6ec3f82974c
blueWisdom09 <758793079@qq.com>
2022-06-21 17:55:41 +0800

    有效的数独

---
+++ algorithm-practice/src/algorithm/array/IsValidSudokuArray.java
@@ -0,0 +1,14 @@
+package algorithm.array;
+
+/**
+ * @author chenjianhua
+ * @Description 有效的数独
+ * @title IsValidSudokeArray
+ * @date 2022/6/21
+ */
+public class IsValidSudokuArray {
+    public boolean isValidSudoku(char[][] board) {
+
+        return true;
+    }
+}
